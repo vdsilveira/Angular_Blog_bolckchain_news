aae50909c66037139bef6d7a3399b37c3f5d8383
vdsilveira <vdsilveira@gmail.com>
2023-07-09 01:52:39 -0300

    propriedades dinamicas de componentes

--- blog/src/app/componentes/bigcard/bigcard.component.ts
+++ blog/src/app/componentes/bigcard/bigcard.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-bigcard',
   templateUrl: './bigcard.component.html',
   styleUrls: ['./bigcard.component.css']
+
+  
 })
-export class BigcardComponent {
+export class BigcardComponent implements OnInit {
+
+  @Input()
+  Fotocapa:string=""
+  @Input()
+  titulopag:string=""
+  @Input()
+  cardDescricao:string=""
+
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
+
+
 
 }
--- blog/src/app/componentes/smallcard/smallcard.component.ts
+++ blog/src/app/componentes/smallcard/smallcard.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-smallcard',
   templateUrl: './smallcard.component.html',
   styleUrls: ['./smallcard.component.css']
 })
-export class SmallcardComponent {
+export class SmallcardComponent  implements OnInit{
+
+
+  @Input()
+  Fotocapa:string=""
+  @Input()
+  titulopag:string=""
+
+  
+
+
+  constructor(){}
+
+  ngOnInit(): void {}
+    
+  
 
 }
